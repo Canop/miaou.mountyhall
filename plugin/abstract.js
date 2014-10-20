a37f51a72de667a28e869b6b447d31ae4734a63e
Canop <cano.petrole@gmail.com>
2014-10-20 08:34:38 +0200

    somme dégâts infligés récemment après la dernière cdm

--- plugin/abstract.js
+++ plugin/abstract.js
@@ -3,50 +3,50 @@
 // Ce code est encore exploratoire, il sera peut-être optimisé plus tard.
 
 var patterns = [
-	{re:/Vous avez utilis. le Sortil.ge : (\w+)/i, res:['sort']},
-	{re:/Le Monstre Cibl. fait partie des :[^\(\)]+\(\s*([^\(]+)\s*-\s*N°(\d+)\)/i, res:['nom','id'], vals:{cdm:'ok'}},
-	{re:/CONNAISSANCE DES MONSTRES sur une?\s+([^\(]+)\s+\((\d{7})\)/i, res:['nom','id'], vals:{cdm:'ok'}},
-	{re:/La Cible subit donc pleinement l'effet/i, vals:{full:true}},
-	{re:/Le sortil.ge a donc un EFFET REDUIT/i, vals:{full:false}},
-	{re:/Le sortil.ge (.*) a eu l'effet/, res:['sort']},
-	{re:/Seuil de R.sistance de la Cible[\._\s]*: (\d+) %/, res:['seuilres']},
+	{re:/ous avez utilis. le Sortil.ge : (\w+)/i, res:['sort']},
+	{re:/e Monstre Cibl. fait partie des :[^\(\)]+\(\s*([^\(]+)\s*-\s*N°(\d+)\)/i, res:['nom','id'], vals:{cdm:'ok'}},
+	{re:/ONNAISSANCE DES MONSTRES sur une?\s+([^\(]+)\s+\((\d{7})\)/i, res:['nom','id'], vals:{cdm:'ok'}},
+	{re:/a Cible subit donc pleinement l'effet/i, vals:{full:true}},
+	{re:/e sortil.ge a donc un EFFET REDUIT/i, vals:{full:false}},
+	{re:/e sortil.ge (.*) a eu l'effet/, res:['sort']},
+	{re:/euil de R.sistance de la Cible[\._\s]*: (\d+) %/, res:['seuilres']},
 	{re:/(\d\d)\/(\d\d)\/(\d{4}) (\d\d):(\d\d):\d\d MORT .* \( (\d+) \) a débarrassé le Monde Souterrain de la présence maléfique d´une? ([^\(]+) \(\s?(\d+)\s?\)/, res:['J','M','A','h','m','killer','nom','id']},
-	{re:/Vous avez attaqu. une? ([^\(]+) \(\s?(\d+)\s?\) .* comp/, res:['nom','id'], vals:{catatt:'comp'}},
-	{re:/Vous avez attaqu. une? ([^\(]+) \(\s?(\d+)\s?\) .* sortil/, res:['nom','id'], vals:{catatt:'sort'}},
-	{re:/Vous avez attaqu. une? ([^\(]+) \(\s?(\d+)\s?\)/, res:['nom','id'], vals:{typeatt:'AN'}},
-	{re:/Le Jet d'Esquive de votre adversaire est de[\._\s]*: (\d+)/, res:['esq']},
-	{re:/Vous avez donc RAT. votre adversair/, vals:{touché:false}},
-	{re:/Vous avez donc TOUCH. votre adversaire par un coup critique/, vals:{touché:true, critique:true}},
-	{re:/Vous avez donc TOUCH. votre adversaire/, vals:{touché:true, critique:false}}, // due to last one
-	{re:/Vous lui avez infligé (\d+) points de dég/, res:['degbrut']},
-	{re:/Son Armure le prot.ge et il ne perdra que (\d+) points de vie/, res:['degnet']},
-	{re:/Il était alors aux alentours de : (\d\d)\/(\d\d)\/(\d{4}) (\d\d):(\d\d):\d\d/, res:['J','M','A','h','m']},
-	{re:/Résultat du Combat : (.*)/, res:['typeatt']},
-	{re:/Votre Jet d'Attaque est de[\._\s]+: (\d+)/, res:['att']},
-	{re:/Le Monstre Cibl. fait partie des : \w+ \(([^\-]+) - N°\s*(\d+)\s*\)/, res:['nom','id']},
-	{re:/Niveau\s*:\s*[^>:\(\)]*\s*\([^\(]+\)/, res:['niveau']},
-	{re:/Blessure\D+(\d+)\s*%/, res:['blessure']},
-	{re:/Armure Physique\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['armurephysmin','armurephysmax']},
-	{re:/Armure Physique\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['armurephysmin']},
-	{re:/Armure Physique\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['armurephysmax'], vals:{armurephysmin:0}},
-	{re:/Armure Magique\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['armuremagmin','armuremagmax'], vals:{aArmure:true}},
-	{re:/Armure Magique\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['armuremagmin'], vals:{aArmure:true}},
-	{re:/Armure Magique\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['armuremagmax'], vals:{armuremagmin:0, aArmure:true}},
-	{re:/Armure\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['armuremin','armuremax'], vals:{aArmure:true}},
-	{re:/Armure\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['armuremin'], vals:{aArmure:true}},
-	{re:/Armure\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['armurepmax'], vals:{armuremin:0, aArmure:true}},
-	{re:/Esquive\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['esquivemin','esquivemax'], vals:{aEsquive:true}},
-	{re:/Esquive\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['esquivemin'], vals:{aEsquive:true}},
-	{re:/Esquive\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['esquivemax'], vals:{esquivemin:0, aEsquive:true}},
-	{re:/Points de Vie\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['pvmin','pvmax'], vals:{aPV:true}},
-	{re:/Points de Vie\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['pvmin'], vals:{aPV:true}},
-	{re:/Points de Vie\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['pvmax'], vals:{pvmin:0, aPV:true}},
-	{re:/Attaque\s*:\s*[^\(]+\(([^\)]+)\)/, res:['att']},
-	{re:/une?\s+([^\(]+)\s+\((\d{7})\) a .t. influenc. par l'effet du sort/i, res:['nom','id']},
-	{re:/Hypnotis.e jusqu'. sa prochaine Date Limite d'Action/i, res:[], vals:{sort:'Hypnotisme'}},
-	{re:/Vous l'avez TU. et avez d.barrass. le Monde Souterrain de sa pr.sence mal.fique/i, vals:{kill:true}},
-	{re:/vous avez gagn. un total de (\d+) PX/, res:['px']},
-	{re:/Il sera, de plus, fragilis. lors des prochaines esquives/i, res:[], vals:{done:true}},
+	{re:/ous avez attaqu. une? ([^\(]+) \(\s?(\d+)\s?\) .* comp/, res:['nom','id'], vals:{catatt:'comp'}},
+	{re:/ous avez attaqu. une? ([^\(]+) \(\s?(\d+)\s?\) .* sortil/, res:['nom','id'], vals:{catatt:'sort'}},
+	{re:/ous avez attaqu. une? ([^\(]+) \(\s?(\d+)\s?\)/, res:['nom','id'], vals:{typeatt:'AN'}},
+	{re:/e Jet d'Esquive de votre adversaire est de[\._\s]*: (\d+)/, res:['esq']},
+	{re:/ous avez donc RAT. votre adversair/, vals:{touché:false}},
+	{re:/ous avez donc TOUCH. votre adversaire par un coup critique/, vals:{touché:true, critique:true}},
+	{re:/ous avez donc TOUCH. votre adversaire/, vals:{touché:true, critique:false}}, // due to last one
+	{re:/ous lui avez infligé (\d+) points de dég/, res:['degbrut']},
+	{re:/on Armure le prot.ge et il ne perdra que (\d+) points de vie/, res:['degnet']},
+	{re:/l était alors aux alentours de : (\d\d)\/(\d\d)\/(\d{4}) (\d\d):(\d\d):\d\d/, res:['J','M','A','h','m']},
+	{re:/ésultat du Combat : (.*)/, res:['typeatt']},
+	{re:/otre Jet d'Attaque est de[\._\s]+: (\d+)/, res:['att']},
+	{re:/e Monstre Cibl. fait partie des : \w+ \(([^\-]+) - N°\s*(\d+)\s*\)/, res:['nom','id']},
+	{re:/iveau\s*:\s*[^>:\(\)]*\s*\([^\(]+\)/, res:['niveau']},
+	{re:/lessure\D+(\d+)\s*%/, res:['blessure']},
+	{re:/rmure Physique\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['armurephysmin','armurephysmax']},
+	{re:/rmure Physique\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['armurephysmin']},
+	{re:/rmure Physique\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['armurephysmax'], vals:{armurephysmin:0}},
+	{re:/rmure Magique\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['armuremagmin','armuremagmax'], vals:{aArmure:true}},
+	{re:/rmure Magique\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['armuremagmin'], vals:{aArmure:true}},
+	{re:/rmure Magique\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['armuremagmax'], vals:{armuremagmin:0, aArmure:true}},
+	{re:/rmure\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['armuremin','armuremax'], vals:{aArmure:true}},
+	{re:/rmure\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['armuremin'], vals:{aArmure:true}},
+	{re:/rmure\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['armurepmax'], vals:{armuremin:0, aArmure:true}},
+	{re:/squive\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['esquivemin','esquivemax'], vals:{aEsquive:true}},
+	{re:/squive\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['esquivemin'], vals:{aEsquive:true}},
+	{re:/squive\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['esquivemax'], vals:{esquivemin:0, aEsquive:true}},
+	{re:/oints de Vie\s*:\s*[^\(]+\(\s*entre\s+(\d+)\s+et\s+(\d+)\s*\)/i, res:['pvmin','pvmax'], vals:{aPV:true}},
+	{re:/oints de Vie\s*:\s*[^\(]+\(\s*sup.rieur\s+.\s+(\d+)\s*\)/i, res:['pvmin'], vals:{aPV:true}},
+	{re:/oints de Vie\s*:\s*[^\(]+\(\s*inf.rieur\s+.\s+(\d+)\s*\)/i, res:['pvmax'], vals:{pvmin:0, aPV:true}},
+	{re:/ttaque\s*:\s*[^\(]+\(([^\)]+)\)/, res:['att']},
+	{re:/ne?\s+([^\(]+)\s+\((\d{7})\) a .t. influenc. par l'effet du sort/i, res:['nom','id']},
+	{re:/ypnotis.e jusqu'. sa prochaine Date Limite d'Action/i, res:[], vals:{sort:'Hypnotisme'}},
+	{re:/ous l'avez TU. et avez d.barrass. le Monde Souterrain de sa pr.sence mal.fique/i, vals:{kill:true}},
+	{re:/ous avez gagn. un total de (\d+) PX/, res:['px']},
+	{re:/l sera, de plus, fragilis. lors des prochaines esquives/i, res:[], vals:{done:true}},
 	{re:/PV : -\d+D\d+ \(-(\d+)\)/, res:['degnet']},
 	{re:/ a .t. tu. par cet effet/, vals:{kill:true}},
 	//~ {re://, res:[]},
@@ -85,6 +85,7 @@ Monster.prototype.addItem = function(item, message){
 	this.items.push(item);
 	this.nom = item.nom;
 	item.message = '['+message.authorname+' '+formatTime(message.created)+'](#'+message.id+')';
+	item.time = message.created;
 }
 // returns a (shared) {min,max} object
 Monster.prototype.reduce = function(name, min, max){
@@ -130,8 +131,10 @@ Monster.prototype.getReportItem = function(o, isAtEnd){
 			if (o.degnet) {
 				r.pv = '**-'+o.degnet+' PV**';
 				if (o.degbrut) r.pv += ' (-'+o.degbrut+')';
+				r.deg = o.degnet;
 			} else {
 				r.pv = '**-'+o.degbrut+' PV**';				
+				r.deg = o.degbrut;
 			}
 			if (o.att) r.détails += 'att: '+o.att+'|';
 			if (o.esq) r.détails += 'esq: '+o.esq+'|';
@@ -177,6 +180,7 @@ Monster.prototype.getReportItem = function(o, isAtEnd){
 		if (/explosion/i.test(o.sort)) {
 			if (!cpl(o, 'degnet')) return;
 			r.pv = '**-'+o.degnet+' PV**';
+			r.deg = o.degnet;
 			// fixme comment déterminer s'il est mort ?
 		}
 		r.action = o.sort
@@ -227,26 +231,48 @@ Monster.prototype.parse = function(message){
 		this.addItem(item, message);
 	}	
 }
+
+Monster.prototype.table = function(){
+	return (
+		"Message|Action|PV|Détails\n"+
+		":-----|:----:|:-:|-------\n"+
+		this.items.map(function(i){
+			return (i.message||' ')+'|'+(i.action||' ')+'|'+(i.pv||' ')+'|'+(i.détails||' ')
+		}).join('\n')
+	);
+}
+
+// if there's an interesting abstracted conclusion, returns it
+Monster.prototype.abstract = function(){
+	var sum = 0, nb = 0, t = 0;
+	for (var i=this.items.length; i--;) {
+		var item = this.items[i];
+		if (item.action==='CDM') break;
+		if (item.deg) {
+			if (t && (t-item.time>86400)) break; // 24h de coupure max
+			sum += +item.deg;
+			nb++;
+		}
+	}
+	if (nb<2) return "";
+	return "\n" + nb + " dernières frappes : **-"+sum+" PV**";
+}
+
 // construit le markdown envoyé à l'utilisateur
 Monster.prototype.mdReport = function(){
+	var r = "*oukonenest* : ";
 	if (this.items.length) {
-		return (
-			"*oukonenest* : " + this.nom + ' ('+this.id+')\n'+
-			"Message|Action|PV|Détails\n"+
-			":-----|:----:|:-:|-------\n"+
-			this.items.map(function(i){
-				return (i.message||' ')+'|'+(i.action||' ')+'|'+(i.pv||' ')+'|'+(i.détails||' ')
-			}).join('\n')
-		);
+		r += this.nom + ' ('+this.id+')\n';
+		r += this.table() + this.abstract();
 	} else {
-		var r = "*oukonenest* : Rien d'intéressant trouvé dans cette salle pour le monstre "+this.id+" (";
+		r += "Rien d'intéressant trouvé dans cette salle pour le monstre " + this.id + " (";
 		switch (this.nbMessages) {
 			case 0:  r += "aucun message ne semble le mentionner)."; break;
 			case 1:  r += "un seul message le mentionne)."; break;
 			default: r += this.nbMessages+" messages)."; 
 		}
-		return r;
 	}
+	return r;
 }
 
 exports.onMonster = function(bot, shoe, id){
