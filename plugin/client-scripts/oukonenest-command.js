05483a3b83ca608724dedbb58390bb14c2bf1ce8
Canop <cano.petrole@gmail.com>
2018-01-04 17:55:55 +0100

    exploite les messages sciz pour l'auto-completion du !!oukonenest

--- plugin/client-scripts/oukonenest-command.js
+++ plugin/client-scripts/oukonenest-command.js
@@ -2,28 +2,40 @@
 // That is: autocompletion
 miaou(function(mountyhall){
 
-	const completionSet = new Set;
+	const	completionSet = new Set,
+		// cette regex est détaillée dans plugin/sciz-oukonenest.js
+		scizEventRegex = /^\d\d\/\d\d\/20\d\d \d\dh\d\d:\d\d [A-zÀ-ÿ]{3,}(?: [A-zÀ-ÿ\[\]]{2,})*(?: \(([^)]+)\))? de ([^(]+) \((\d+)\) sur ([^(]+) \((\d+)\)/,
+		scizBotName = "SCIZ-Bot";
 
 	const cachetteRegexes = [
 		/la cachette de la carte \((\d+)\)\./,
 		/Carte de la Cachette # (\d+)/
 	];
 
-	mountyhall.seeMessage = function(m){
-		const c = m.content;
+	mountyhall.seeMessage = function(message){
+		const c = message.content;
 		if (!c) return;
+		// message SCIZ
+		if (message.authorname==scizBotName) {
+			var match = message.content.match(scizEventRegex);
+			console.log('match:', match);
+			if (match) {
+				completionSet.add(match[4]+" ("+match[5]+")");
+			}
+			return;
+		}
 		// trolls & monstres
 		for (var i=mountyhall.searchRegexes.length; i--;) {
-			var m = c.match(mountyhall.searchRegexes[i]);
-			if (m) {
-				completionSet.add(m[1]+" ("+m[2]+")");
+			var match = c.match(mountyhall.searchRegexes[i]);
+			if (match) {
+				completionSet.add(match[1]+" ("+match[2]+")");
 			}
 		}
 		// cachettes de capitan
 		for (var i=cachetteRegexes.length; i--;) {
-			var m = c.match(cachetteRegexes[i]);
-			if (m) {
-				completionSet.add("carte " + m[1]);
+			var match = c.match(cachetteRegexes[i]);
+			if (match) {
+				completionSet.add("carte " + match[1]);
 			}
 		}
 	}
