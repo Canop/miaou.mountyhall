02e82c3350a93daf5f89cbaa72c26b2d3e0983a1
Canop <cano.petrole@gmail.com>
2020-01-03 07:02:43 +0100

    fix owner role not recognized in partage

--- plugin/client-scripts/partage.js
+++ plugin/client-scripts/partage.js
@@ -1,6 +1,6 @@
 miaou(function(mountyhall, chat, gui, locals, time, ws){
 
-	const isAdmin = locals.room && (locals.room.auth==="admin" || locals.room.auth==="own");
+	const isAdmin = locals.room && (locals.room.auth==="admin" || locals.room.auth==="owner");
 
 	mountyhall.partage = {};
 
