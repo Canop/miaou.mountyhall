d247f16169b295ca6a83b0dbe72472f46d72d57a
Canop <cano.petrole@gmail.com>
2025-02-06 17:24:24 +0100

    update script as ftp access isn't available anymore

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 rm Public_Trolls.txt-save
 mv Public_Trolls.txt Public_Trolls.txt-save
-lftp ftp://ftp.mountyhall.com/ -e "get /Public_Trolls.txt -o Public_Trolls.txt; quit"
+wget https://ftp.mountyhall.com/Public_Trolls.txt
 node update.js
 
